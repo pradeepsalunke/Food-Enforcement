7bfee8772cb5440532831e6e2958642168603f36
Alysson Silveira <alysson.osilveira@gmail.com>
2019-11-23 19:22:06 -0500

    Ignore

--- Models/RootObject.cs
+++ Models/RootObject.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Food.Models
 {
-    // Model for parks data. These classes can be obtained by either using 
-    // the Visual Studio editor (right-click -> Paste Special -> Paste Json as Classes)
-    // or sites such as JsonToCSHarp
+    public class MyContext: DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options)
+            :base(options)
+        { }
 
+        public DbSet<Result> RootObjects { get; set; }
+    }
     public class Results
     {
         public int skip { get; set; }
@@ -30,30 +36,55 @@ namespace Food.Models
     public class Result
     {
         public string country { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string city { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string reason_for_recall { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string address_1 { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string address_2 { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string code_info { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string product_quantity { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string center_classification_date { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string distribution_pattern { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string state { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string product_description { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string report_date { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string classification { get; set; }
-        public Openfda openfda { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
+        public string openfda { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string recall_number { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string recalling_firm { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string initial_firm_notification { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string event_id { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string product_type { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string termination_date { get; set; }
-        public object more_code_info { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
+        public string more_code_info { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string recall_initiation_date { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string postal_code { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string voluntary_mandated { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string status { get; set; }
+
     }
 
     public class RootObject
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Food.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Food
 {
@@ -33,6 +35,10 @@ namespace Food
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=RootObjects;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<MyContext>
+                (options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
